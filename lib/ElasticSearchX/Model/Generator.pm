9abdf9cfbe6bd99ca782bddde33e99ccddefd16c
Kent Fredric <kentfredric@gmail.com>
2012-08-10 18:29:20 +1200

    Build results of 7d32356 (on master)

--- lib/ElasticSearchX/Model/Generator.pm
+++ lib/ElasticSearchX/Model/Generator.pm
@@ -29,33 +29,41 @@ use Sub::Exporter -setup => {
 use MooseX::Has::Sugar qw( rw ro required );
 use Sub::Quote qw( quote_sub );
 
+
 has mapping_url => rw, required;
 has base_dir    => rw, required;
 
+
 has generator_base_class => rw, default => quote_sub(q{ 'ElasticSearchX::Model::Generator' });
 has generated_base_class => rw, default => quote_sub(q{ 'MyModel' });
+
 
 has document_generator_class  => is => lazy =>,;
 has attribute_generator_class => is => lazy =>,;
 has typename_translator_class => is => lazy =>,;
 
+
 has document_generator  => is => lazy =>,;
 has attribute_generator => is => lazy =>,;
 has typename_translator => is => lazy =>,;
 
+
 has _mapping_content => is => lazy =>,;
 has _ua              => is => lazy =>,;
 has _mapping_data    => is => lazy =>,;
+
 
 sub _build__ua {
   require HTTP::Tiny;
   return HTTP::Tiny->new();
 }
 
+
 sub _build_document_generator_class {
   my $self = shift;
   return $self->generator_base_class . '::DocumentGenerator';
 }
+
 
 sub _build_document_generator {
   my $self = shift;
@@ -63,10 +71,12 @@ sub _build_document_generator {
   return Module::Runtime::use_module( $self->document_generator_class )->new( generator_base => $self, );
 }
 
+
 sub _build_attribute_generator_class {
   my $self = shift;
   return $self->generator_base_class . '::AttributeGenerator';
 }
+
 
 sub _build_attribute_generator {
   my $self = shift;
@@ -74,16 +84,19 @@ sub _build_attribute_generator {
   return Module::Runtime::use_module( $self->attribute_generator_class )->new( generator_base => $self );
 }
 
+
 sub _build_typename_translator_class {
   my $self = shift;
   return $self->generator_base_class . '::TypenameTranslator';
 }
+
 
 sub _build_typename_translator {
   my $self = shift;
   require Module::Runtime;
   return Module::Runtime::use_module( $self->typename_translator_class )->new( generator_base => $self );
 }
+
 
 sub _build__mapping_content {
   my $self     = shift;
@@ -103,16 +116,20 @@ sub _build__mapping_content {
   return $response->{content};
 }
 
+
 sub _build__mapping_data {
   my $self    = shift;
   my $content = $self->_mapping_content;
   require JSON;
   return JSON->new()->utf8(1)->decode($content);
 }
+
+
 ## no critic ( RequireArgUnpacking ProhibitBuiltinHomonyms )
 sub index_names {
   return keys %{ $_[0]->_mapping_data };
 }
+
 
 sub index {
   if ( $_[1] eq q{} ) {
@@ -121,30 +138,36 @@ sub index {
   return $_[0]->_mapping_data->{ $_[1] };
 }
 
+
 sub type_names {
   my ( $self, $index ) = @_;
   return keys %{ $self->index($index) };
 }
+
 
 sub type {
   my ( $self, $index, $type ) = @_;
   return $self->index($index)->{$type};
 }
 
+
 sub property_names {
   my ( $self, $index, $type ) = @_;
   return keys %{ $self->properties( $index, $type ) };
 }
+
 
 sub properties {
   my ( $self, $index, $type ) = @_;
   return $self->type( $index, $type )->{properties};
 }
 
+
 sub property {
   my ( $self, $index, $type, $property ) = @_;
   return $self->properties( $index, $type )->{$property};
 }
+
 
 sub documents {
   my ( $self, @indices ) = @_;
@@ -195,6 +218,145 @@ version 0.1.0
 =head1 DESCRIPTION
 
 B<ALPHA Code>: This class at present only contains code sufficient for very simple package generation for use in creating a model from an existing mapping for the purposes of search. 
+
+=head1 METHODS
+
+=head2 index_names
+
+  @names = $esmg->index_names
+
+returns the names of all indices specified in the C<_mapping>
+
+=head2 index
+
+  $data = $esmg->index('') # If indices are not in the dataset 
+  $data = $esmg->index('cpan_v1') # if indices are in the dataset 
+
+Returns the dataset nested under the specified index. 
+
+=head2 type_names
+
+  @names = $esmg->type_names( $index )
+  @names = $esmg->type_names('')  # return all types defined in an index-free dataset.
+  @names = $esmg->type_names('cpan_v1') # return all types in the cpan_v1 index.
+
+=head2 type
+
+  $data = $esmg->type( $index, $type )
+  $data = $esmg->type( '', 'File' )    # get type 'File' from an index-free dataset
+  $data = $esmg->type( 'cpan_v1', 'File' )    # get type 'File' from the cpan_v1 index
+
+=head2 property_names
+
+  @names = $esmg->property_names( $index, $type )
+
+=head2 properties
+
+  $properties = $esmg->properties( $index, $type )
+
+=head2 property
+
+  $property = $esmg->property( $index, $type, $propertyname )
+
+=head2 documents
+
+  @documents = $esmg->documents(); # all documents for all indices 
+  @documents = $esmg->documents('cpan_v1'); # all documents for cpan_v1
+  @documents = $esmg->documents(''); # all documents for an index-free dataset.
+
+=head1 ATTRIBUTES
+
+=head2 mapping_url
+
+  rw, required
+
+=head2 base_dir
+
+  rw, required
+
+=head2 generator_base_class
+
+  rw, default: ElasticSearchX::Model::Generator
+
+=head2 generated_base_class
+
+  rw, default: MyModel
+
+=head2 document_generator_class
+
+  lazy
+
+=head2 attribute_generator_class
+
+  lazy
+
+=head2 typename_translator_class
+
+  lazy
+
+=head2 document_generator 
+
+  lazy
+
+=head2 attribute_generator
+
+  lazy
+
+=head2 typename_translator
+
+  lazy
+
+=head1 PRIVATE ATTRIBUTES
+
+=head2 _mapping_content
+
+  lazy
+
+=head2 _ua
+
+  lazy
+
+=head2 _mapping_data
+
+  lazy
+
+=head1 PRIVATE METHODS
+
+=head2 _build__ua
+
+returns an C<HTTP::Tiny> instance.
+
+=head2 _build_document_generator_class
+
+  generator_base_class + '::DocumentGenerator'
+
+=head2 _build_document_generator
+
+returns an instance of C<$document_generator_class>
+
+=head2 _build_attribute_generator_class
+
+  generator_base_class + '::AttributeGenerator'
+
+=head2 _build_attribute_generator
+
+returns an instance of C<$attribute_generator_class>
+
+=head2 _build_typename_translator_class
+
+  generator_base_class + '::TypenameTranslator'
+
+=head2 _build_typename_translator
+
+returns an instance of C<$typename_translator_class>
+
+=head2 _build__mapping_content 
+
+returns the content of the url at C<mapping_url>
+
+=head2 _build__mapping_data
+
+returns the decoded data from JSON stored in C<_mapping_content>
 
 =head1 AUTHOR
 
